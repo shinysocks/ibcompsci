ffaac86be6e2b16fa3b2b06d0dc7f59f132248eb
Noah Dinan <noah.dinan@gmail.com>
2023-01-04 21:23:49 -0600

    added Classes and Objects

---
+++ Classes and Objects/src/Circle.java
@@ -0,0 +1,17 @@
+public class Circle {
+	//states - instance variable
+	int centerX;
+	int centerY;
+	int radius;
+	
+	//constructor
+	public Circle(int theX, int theY, int theRadius)
+	{
+		centerX = theX;
+		centerY = theY;
+		radius = theRadius;
+	}
+	
+	//methods - behavior
+
+}
---
+++ Classes and Objects/src/CircleMaker.java
@@ -0,0 +1,10 @@
+
+public class CircleMaker {
+
+	public static void main(String[] args)
+	{
+		Circle c1 = new Circle(0, 0, 10);
+		System.out.println(Math.PI);
+	}
+
+}
